3dba7fb6026583b1ddfac11974ea184712b747c5
gage1145 <gage.rowden1145@gmail.com>
2025-08-29 16:48:10 -0500

    Images write to memory instead of tempfiles now.

--- ascii_video.py
+++ ascii_video.py
@@ -1,39 +1,29 @@
-import PIL.Image
 import cv2
 import os
 import sys
 import time
-
-
+from PIL import Image
 
 # ascii characters used to build the output text
 ASCII_CHARS = [i for i in """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft\|(1}[?-_+~<i!lI;:,"^'. """][::-1]
 ASCII_CHARS_SHORT = [i for i in "@#$%?*+;:,. "][::-1]
 
-# Convert video frames to individual images
+
+# convert video into list of PIL images
 def convert_video(file):
     cap = cv2.VideoCapture(file)
-    try:
-        if not os.path.exists("images"):
-            os.makedirs("images")
-    except OSError:
-        print("Error: Creating directory of images.")
-    
-    current_frame = 1
+    frames = []
     while True:
-        # Capture frame-by-frame
         ret, frame = cap.read()
-
-        # Saves image of the current frame in jpg file
-        name = "./images/frame" + "_" + str(current_frame) + ".jpg"
-        try:
-            cv2.imwrite(name, frame)
-            print("Creating..." + name, end="\r")
-            current_frame += 1
-        except:
+        if not ret:
             break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        pil_image = Image.fromarray(frame_rgb)
+        frames.append(pil_image)
     cap.release()
     cv2.destroyAllWindows()
+    return frames
+
 
 # resize image according to a new width
 def resize_image(image, new_width=100):
@@ -41,59 +31,66 @@ def resize_image(image, new_width=100):
     ratio = height / (1.9 * width)
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
-    return(resized_image)
+    return resized_image
+
 
 def change_contrast(img, level=100):
     factor = (259 * (level + 255)) / (255 * (259 - level))
+
     def contrast(c):
         return 128 + factor * (c - 128)
+
     return img.point(contrast)
+
 
 # convert each pixel to grayscale
 def grayify(image):
-    grayscale_image = image.convert("L")
-    return(grayscale_image)
-    
+    return image.convert("L")
+
+
 # convert pixels to a string of ascii characters
 def pixels_to_ascii(image):
     pixels = image.getdata()
-    characters = "".join([ASCII_CHARS_short[pixel//22] for pixel in pixels])
-    return(characters)    
+    characters = "".join([ASCII_CHARS_SHORT[pixel // 22] for pixel in pixels])
+    return characters
 
-def main(path, new_width=100, contrast=100):
-    try:
-        image = PIL.Image.open(path)
-    except:
-        print(path, " is not a valid pathname to an image.")
-        return
-  
+
+def image_to_ascii(image, new_width=75, contrast=0):
     # convert image to ascii    
-    new_image_data = pixels_to_ascii(grayify(change_contrast(resize_image(image, new_width=new_width), contrast)))
-    
+    new_image_data = pixels_to_ascii(
+        grayify(
+            change_contrast(
+                resize_image(image, new_width=new_width),
+                contrast
+            )
+        )
+    )
+
     # format
-    pixel_count = len(new_image_data)  
-    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
-    
-    # Return result
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(
+        [new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)]
+    )
+
     return ascii_image
+
 
 if __name__ == "__main__":
     nargs = len(sys.argv)
     if nargs != 2:
         raise ValueError(f"Expected only 1 argument. Received {nargs}")
     file = sys.argv[1]
-    if not os.listdir("images"):
-        if not os.path.exists(file):
-            raise ValueError(f"File {file} does not exist. ")
-        convert_video(file)
+    if not os.path.exists(file):
+        raise ValueError(f"File {file} does not exist.")
 
-    movie = []
-    images = os.listdir("images")
-    for i in range(len(images)):
-        image = os.path.join("images", images[i])
-        movie.append(main(image, 200, 0))
+    # grab frames directly in memory
+    frames = convert_video(file)
 
-    for i in range(len(images)):
-        os.system("cls")
-        print(movie[i])
-        time.sleep(0.0167)
+    # convert all frames to ascii strings
+    movie = [image_to_ascii(frame, 100, 0) for frame in frames]
+
+    # play ascii animation
+    for ascii_frame in movie:
+        os.system("cls" if os.name == "nt" else "clear")
+        print(ascii_frame)
+        time.sleep(0.0167)  # ~60 fps
